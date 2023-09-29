50e4db06c658568faa91198e078d804f579dd475
Malcolm Caleb <malcolmcaleb01@gmail.com>
2023-09-29 16:40:07 -0400

    added user input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+const validateSignup = [
+    check('email')
+        .exists({ checkFalsy: true})
+        .isEmail()
+        .withMessage('Please provide a valid email.'),
+    check('username')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email.'),
+    check('password')
+        .exists({ checkFalsy:true })
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+];
+
+router.post('/', validateSignup, async (req, res) => {
     const { email, password, username } = req.body;
     const hashedPassword = bcrypt.hashSync(password);
     const user = await User.create({ email, username, hashedPassword });
@@ -32,6 +54,19 @@ router.post('/', async (req, res) => {
 // })
 
 module.exports = router;
+
+// fetch('/api/users/', {
+//     method: 'POST',
+//     headers: {
+//         "Content-Type": "application/json",
+//         "XSRF-TOKEN": "VjiE4RvO--H_iltZZVjWzaP_A96_6hipEnkk"
+//     },
+//     body: JSON.stringify({
+//         email: 'wehatepython@javascript.com',
+//         username: 'welovejava',
+//         password:'unoar'
+//     })
+// }).then(res => res.json()).then(data => console.log(data))
 
 
 // fetch('/api/users/users', {
