fe20d950240943b086b157a9469fd1783910116a
Malcolm Caleb <malcolmcaleb01@gmail.com>
2023-10-02 19:14:34 -0400

    added review image seeders

--- backend/db/seeders/20231002223720-demo-review.js
+++ backend/db/seeders/20231002223720-demo-review.js
@@ -1,34 +1,39 @@
-// 'use strict';
+'use strict';
 
-// /** @type {import('sequelize-cli').Migration} */
+/** @type {import('sequelize-cli').Migration} */
 
-// const { User } = require('../models');
-// const bcrypt = require('bcryptjs');
+const { Review } = require('../models');
+const bcrypt = require('bcryptjs');
 
-// let options = {};
-// if (process.env.NODE_ENV === 'production') {
-//   options.schema = process.env.SCHEMA;
-// }
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
 
-// module.exports = {
-//   async up (queryInterface, Sequelize) {
-//     /**
-//      * Add seed commands here.
-//      *
-//      * Example:
-//      * await queryInterface.bulkInsert('People', [{
-//      *   name: 'John Doe',
-//      *   isBetaMember: false
-//      * }], {});
-//     */
-//   },
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await Review.bulkCreate([
+      {
 
-//   async down (queryInterface, Sequelize) {
-//     /**
-//      * Add commands to revert seed here.
-//      *
-//      * Example:
-//      * await queryInterface.bulkDelete('People', null, {});
-//      */
-//   }
-// };
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
--- backend/db/seeders/20231002223734-demo-review-image.js
+++ backend/db/seeders/20231002223734-demo-review-image.js
@@ -1,34 +1,52 @@
-// 'use strict';
+'use strict';
 
-// /** @type {import('sequelize-cli').Migration} */
+/** @type {import('sequelize-cli').Migration} */
 
-// const { User } = require('../models');
-// const bcrypt = require('bcryptjs');
+const { ReviewImage } = require('../models');
+const bcrypt = require('bcryptjs');
 
-// let options = {};
-// if (process.env.NODE_ENV === 'production') {
-//   options.schema = process.env.SCHEMA;
-// }
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
 
-// module.exports = {
-//   async up (queryInterface, Sequelize) {
-//     /**
-//      * Add seed commands here.
-//      *
-//      * Example:
-//      * await queryInterface.bulkInsert('People', [{
-//      *   name: 'John Doe',
-//      *   isBetaMember: false
-//      * }], {});
-//     */
-//   },
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await ReviewImage.bulkCreate([
+    {
+      reviewId: 1,
+      url: 'https://github.com/malcolmc22/Express_Project/blob/main/images/airbnb_dbdiagram.png'
+    },
+    {
+      reviewId: 2,
+      url: 'https://github.com/malcolmc22/Express_Project/blob/main/images/airbnb_dbdiagram.png'
+    },
+    {
+      reviewId: 3,
+      url: 'https://github.com/malcolmc22/Express_Project/blob/main/images/airbnb_dbdiagram.png'
+    }
+   ], {validate: true })
+  },
 
-//   async down (queryInterface, Sequelize) {
-//     /**
-//      * Add commands to revert seed here.
-//      *
-//      * Example:
-//      * await queryInterface.bulkDelete('People', null, {});
-//      */
-//   }
-// };
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'ReviewImages'
+    await queryInterface.bulkDelete(options, {
+      reviewId: [1,2,3]
+    })
+  }
+};
