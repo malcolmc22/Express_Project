6da8b4085e5a814390a50190f9eef84757eeefe4
Malcolm Caleb <malcolmcaleb01@gmail.com>
2023-10-20 18:53:58 -0400

    added signup modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormModal";
+import { Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
-import SignupFormPage from "./components/SignupFormPage";
 import Navigation from "./components/Navigation";
 
 function App() {
@@ -16,16 +14,7 @@ function App() {
   return (
     <>
       <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
-      <Switch>
-        {/* <Route path="/login">
-          <LoginFormPage />
-        </Route> */}
-        <Route path="/signup">
-          <SignupFormPage />
-        </Route>
-      </Switch>
-      )}
+      {isLoaded && <Switch></Switch>}
     </>
   );
 }
