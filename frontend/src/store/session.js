432ec7ef5c1ae2659f7e9f75675859fa61ab13d2
Malcolm Caleb <malcolmcaleb01@gmail.com>
2023-10-20 14:00:28 -0400

    implemented log in

---
+++ frontend/src/store/session.js
@@ -0,0 +1,56 @@
+import { csrfFetch } from "./csrf";
+
+const SET_SESSION = 'session/setSessionUser';
+const REMOVE_SESSION = 'session/removeSessionUser';
+
+export const setSessionUser = (user) => {
+    return {
+        type: SET_SESSION,
+        user
+      };
+}
+
+export const removeSessionUser = () => {
+    return {
+        type: REMOVE_SESSION,
+    }
+}
+
+export const setSessionUserThunk = (user) => async (dispatch) => {
+    const { credential, password } = user
+    const res = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential,
+            password
+        })
+    })
+
+    if (res.ok) {
+        const data = await res.json()
+        console.log(data)
+        dispatch(setSessionUser(data.user))
+        return res
+    }
+}
+
+const initialState = { user: null }
+
+const sessionReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_SESSION:{
+            const newState = {...state}
+            newState.user = action.user
+            return newState;
+        }
+        case REMOVE_SESSION:{
+             const newState = {};
+             newState.user = null;
+            return newState
+        }
+        default:
+            return state;
+    }
+}
+
+export default sessionReducer;
