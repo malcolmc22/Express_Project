b0ebc5c3505d45b147ad9761e3aba3d7ff9d26cc
Malcolm Caleb <malcolmcaleb01@gmail.com>
2023-10-29 15:06:50 -0400

    most of create review css done

--- frontend/src/components/Reviews/CreateReview.js
+++ frontend/src/components/Reviews/CreateReview.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
+import {
+  useHistory,
+  useParams,
+} from "react-router-dom/cjs/react-router-dom.min";
 import { makeReviewThunk } from "../../store/reviews";
 import { useModal } from "../../context/Modal";
 function CreateReview() {
@@ -21,46 +24,63 @@ function CreateReview() {
   }
   const spotId = useSelector((state) => Object.values(state.spots.spots)[0].id);
   // console.log('spot', spots)
-  useEffect(() => {
-
-  }, [dispatch])
+  useEffect(() => {}, [dispatch]);
   const onSubmit = async (e) => {
     // e.preventDefault()
     const createdReview = {
       review,
-      stars
-    }
-    const newReview = await dispatch(makeReviewThunk(createdReview, spotId))
-    if(newReview.id) {
-      closeModal()
+      stars,
+    };
+    const newReview = await dispatch(makeReviewThunk(createdReview, spotId));
+    if (newReview.id) {
+      closeModal();
     } else {
-      setErrors(newReview)
+      setErrors(newReview);
       // console.log(newReview)
     }
     // console.log(newReview,'new review')
     // if(!newReview) {
     //   setErrors(newReview)
     // }
-  }
+  };
   return (
-    <form onSubmit={onSubmit}>
-      <h1>How was your stay?</h1>
+    <form className="review-modal" onSubmit={onSubmit}>
+      <div className="create-review-title">How was your stay?</div>
       {errors.errors && <div>{errors.errors.review}</div>}
-      <input
+      <textarea
         type="text"
+        className="review-input"
         placeholder="Leave your review here..."
         value={review}
         onChange={(e) => setReview(e.target.value)}
       />
       <div className="stars-container">
-        <i className="fa-solid fa-star fa-lg" />
-        <div className="up-down-container">
-            <i onClick={(e) => setStars(stars === 5 ? 5 : stars + 1)} className="fa-solid fa-caret-up" />
-            <i onClick={(e) => setStars(stars === 1 ? 1 : stars - 1)} className="fa-solid fa-caret-down" />
-            {<div> Stars: {stars} </div>}
+        <div className="star-count-icon-container">
+          <div className="star-icon-container">
+            <i className="fa-solid fa-star fa-lg" />
+          </div>
+          {<div className="star-count-container"> Stars: {stars} </div>}
         </div>
-        <button type="submit" disabled={review.length < 10 || stars < 1}>Submit Your Review</button>
+        <div className="up-down-container">
+          <div className="up-arrow-container">
+            {" "}
+            <i
+              onClick={(e) => setStars(stars === 5 ? 5 : stars + 1)}
+              className="fa-solid fa-arrow-up"
+            />
+          </div>
+          <div className="down-arrow-container">
+            <i
+              onClick={(e) => setStars(stars === 1 ? 1 : stars - 1)}
+              className="fa-solid fa-arrow-down"
+            />
+          </div>
+          {/* {<div className="star-count-container"> Stars: {stars} </div>} */}
+        </div>
       </div>
+      <button type="submit" disabled={review.length < 10 || stars < 1}>
+        Submit Your Review
+      </button>
     </form>
   );
 }
