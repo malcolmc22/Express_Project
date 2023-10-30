2b3b3e8c09b4706068df2bd19d97833982555fd9
Malcolm Caleb <malcolmcaleb01@gmail.com>
2023-10-29 23:05:15 -0400

    finished last of css

--- frontend/src/components/Spots/SpotById.js
+++ frontend/src/components/Spots/SpotById.js
@@ -6,7 +6,7 @@ import * as reviewActions from "../../store/reviews"
 import OpenModalButton from "../OpenModalButton";
 import CreateReview from "../Reviews/CreateReview";
 import { useHistory } from "react-router-dom";
-
+import './SpotById.css'
 const SpotbyId = () => {
   const history = useHistory();
   const [isLoaded, setIsLoaded] = useState(false)
@@ -45,40 +45,45 @@ const SpotbyId = () => {
   if (isLoaded === false) return null;
   if (!reviews[0] === null ) return null;
   return (
-    <div>
+    <div >
       {isLoaded && reviewsLoaded && spots[0] && (
-        <div>
-          <h1>{spots[0].name}</h1>
-          <h3>{spots[0].city},{spots[0].state},{spots[0].country} </h3>
-          <div className="image-container">
-            {spots[0].SpotImages && spots[0].SpotImages.map(({url}) => (
+        <div className="spot-by-id-container">
+          <div className="spot-by-id-title">{spots[0].name}</div>
+          <div className="spot-by-id-header">{spots[0].city},{spots[0].state},{spots[0].country} </div>
+
+
+            <div className="image-container">
+            {spots[0].SpotImages && spots[0].SpotImages.map(({url, preview}) => (
               // {console.log(image.url, 'img')}
-              <img key={url} src={url} />
+              <div className={`spot-by-id-image-${preview}`}> <img key={url} src={url} /> </div>
             ))}
           </div>
-          <div className="info-container">
-              <h2>Hosted by { isLoaded && spots[0].Owner.firstName} { isLoaded && spots[0].Owner.lastName}</h2>
-              <div>{spots[0].description}</div>
+
+          {/* <div className="info-container"> */}
+              <div className="host-container">Hosted by { isLoaded && spots[0].Owner.firstName} { isLoaded && spots[0].Owner.lastName}</div>
+              <div className="spot-by-id-description-container">{spots[0].description}</div>
               <div className="reserve-container">
-                <div>${spots[0].price}night</div>
-                <div> <i className="fa-solid fa-star"></i>{spots[0].avgRating?.toFixed(1)}</div>
-                <div>{ spots[0].numReviews > 1 ? `· ${spots[0].numReviews} Reviews` : spots[0].numReviews === 1 ? `· ${spots[0].numReviews} Review` : 'New'}</div>
-                <button onClick={() => alert('Feature Coming Soon...')}> Reserve </button>
+                <div className="stars-reviews-containers">
+                <div className="spot-by-id-price">${spots[0].price} night</div>
+                <div className="spot-by-id-star-container"> <i className="fa-solid fa-star"></i>{spots[0].avgRating?.toFixed(1)}</div>
+                <div className="spot-by-id-reviews">{ spots[0].numReviews > 1 ? `· ${spots[0].numReviews} Reviews` : spots[0].numReviews === 1 ? `· ${spots[0].numReviews} Review` : 'New'}</div>
+{/* </div> */}
+                </div>
+                <button className='reserve-button' onClick={() => alert('Feature Coming Soon...')}> Reserve </button>
               </div>
-          </div>
+              {/* <div className="divider"> </div> */}
           <div className="reviews-container">
             {currUserId && !(reviews.find((review) => review.User.id === currUserId.id)) && spots[0].ownerId !== currUserId.id && <OpenModalButton spotId={spotId} buttonText='Post Your Review' modalComponent={<CreateReview />}/>}
-            <h3> <i className="fa-solid fa-star"></i>{spots[0].avgRating?.toFixed(1)} { spots[0].numReviews > 1 ? `· ${spots[0].numReviews} Reviews` : spots[0].numReviews === 1 ? `· ${spots[0].numReviews} Review` : 'New'}</h3>
+            <div className="spot-by-id-reviews-count"> <div className="star-and-reviews"><i className="fa-solid fa-star"></i>{spots[0].avgRating?.toFixed(1)} { spots[0].numReviews > 1 ? `· ${spots[0].numReviews} Reviews` : spots[0].numReviews === 1 ? `· ${spots[0].numReviews} Review` : 'New'}</div></div>
               {reviewsLoaded && reviews[0] && reviews?.map((review) => (
-
                 <div key={review?.id} className="review-container">
-                 <div>{review.User.firstName} </div>
-                 <div>{new Date(review.createdAt).toDateString()}</div>
-                 <div>{review.review}</div>
-                 {review?.userId === currUserId?.id && <button onClick={() => history.push(`/spots/${spotId}/reviews/${review?.id}/delete`)}>Delete</button>}
+                 <div className="reviewers-firstname-container">{review.User.firstName} </div>
+                 <div className="review-created-container">{new Date(review.createdAt).toDateString()}</div>
+                 <div className="spot-by-id-review-container">{review.review}</div>
+                 {review?.userId === currUserId?.id && <button className='spot-by-id-delete-review-button' onClick={() => history.push(`/spots/${spotId}/reviews/${review?.id}/delete`)}>Delete</button>}
                 </div>
               )).sort((a,b) => a.createdAt - b.createdAt)}
-              {!reviews[0] && spots[0].ownerId !== currUserId?.id && <div>Be the first to post a review!</div>}
+              {!reviews[0] && spots[0].ownerId !== currUserId?.id && <div className="no-reviews-container">Be the first to post a review!</div>}
           </div>
         </div>
       )}
